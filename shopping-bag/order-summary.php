0673570c3ce4864f9d2fe67490d00b668178b575
Alessandra Milburn <57661880+AlessandraMilburn@users.noreply.github.com>
2020-05-24 13:33:35 +0400

    created static order-summary component

---
+++ shopping-bag/order-summary.php
@@ -0,0 +1,12 @@
+<div class="container">
+  <h6>ORDER SUMMARY</h6>
+  <p>Subtotal <span>$123.00</span>
+  <p>Shipping <span>$123.00</span>
+  <span>line</span>
+  <p>Total <span>$123.00</span>
+  <span>Change currency</span>
+
+  <h6>NOTES</h6>
+  <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</p>
+  <button>Go to checkout</button>
+</div>
