31708533cb3b817512bd05f90eff27b2f7bdbf29
Alessandra Milburn <57661880+AlessandraMilburn@users.noreply.github.com>
2020-05-31 16:18:15 +0400

    fixed #payment-methods excess margin

--- components/payment-methods.php
+++ components/payment-methods.php
@@ -1,7 +1,7 @@
-<div class="container-fluid m-1">
+<div class="container-fluid py-1">
   <div class="row p-0">
     <div class="col-3"></div>
-    <div class="col-6 d-flex justify-content-center" id="payment-methods">
+    <div class="col-6 d-flex justify-content-center " id="payment-methods">
       <p class="d-flex align-items-center">Payment Methods:</p>
       <img src="/wp-content/uploads/2020/05/paypal.svg"/>
       <img src="/wp-content/uploads/2020/05/mastercard.svg"/>
