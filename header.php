853a30c1b006538cf322a9b018f642c7c172379d
Alessandra Milburn <57661880+AlessandraMilburn@users.noreply.github.com>
2020-06-07 14:27:08 +0400

    updated header styling and moved announcement bar to the top

--- header.php
+++ header.php
@@ -19,6 +19,10 @@
 **Theme Menu
 -->
     <header>
+      <!--ANNOUNCEMENT SECTION-->
+      <div class="announcement bg-black">
+        <p class="mb-0 white">Check out our new collection - just in! <a><u>Click here</u></a></p>
+      </div>
       <nav class="container-fluid" id="nav" style="background-image: url('<?php header_image(); ?>');" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>">
         <div class="row">
           <div class="col-5"></div>
@@ -30,25 +34,25 @@
             <div class="container py-3">
               <div class="row d-flex justify-content-end">
                 <div class="col-3"></div>
-                <div class="col-1 p-0">
+                <div class="col-1 p-1">
                   <img src="/wp-content/uploads/2020/05/icon-user.svg"/>
                 </div>
-                <div class="col-1 p-0 mx-1">
+                <div class="col-1 p-1">
                   <img src="/wp-content/uploads/2020/05/icon-heart-1.svg"/>
                 </div>
-                <div class="col-1 p-0">
+                <div class="col-1 p-1">
                   <img src="/wp-content/uploads/2020/05/icon-cart.svg"/>
                 </div>
-                <div class="col-4 py-1 px-0"><span class="bold-text">(1)</span> ($123.00)</div>
+                <div class="col-4 py-3 px-0 xs-text"><span class="bold-text ">(1)</span> ($123.00)</div>
               </div>
             </div>
           </div>
         </div>
-        <div class="row mb-0 py-1">
+        <div class="row mb-0 py-1 ">
           <div class="col-2"></div>
           <div class="col-8">
             <div class="container">
-              <div class="row mb-0 hover nav-button">
+              <div class="row mb-0 hover nav-button reg-text ">
                 <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) );?>
                 <div class="col-2"><a>SEARCH BAR</a></div>  
               </div>     
@@ -59,9 +63,6 @@
         </div>
       </nav>
 
-      <!--ANNOUNCEMENT SECTION-->
-      <div class="announcement bg-light-grey">
-        <p class="mb-0">Check out our new collection - just in! <a><u>Click here</u></a></p>
-      </div>
+
     </header>
 <main>
